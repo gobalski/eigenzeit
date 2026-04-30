6bc7f7bbec303674c0f0fc78241cb34c63469c47
gobalski <purpl@posteo.net>
2026-04-30 11:18:58 +0200

    reset functionality

--- src/main.c
+++ src/main.c
@@ -6,7 +6,6 @@
 #include "libpp.h"
 
 static const int DELAY = 800;
-
 static i2c_inst_t *I2C = i2c0;
 static const uint8_t FRAM_ADDR = 0x50;
 static const char UPTIME_ADDR[2] = {0x0, 0x0};
@@ -35,8 +34,6 @@ int init_i2c_bus() {
     // Initialize I2C pins
     gpio_set_function(sda_pin, GPIO_FUNC_I2C);
     gpio_set_function(scl_pin, GPIO_FUNC_I2C);
-
-    // Check if Device sends ACK after sending address
 }
 
 int writeTime(int64_t uptime) {
@@ -51,11 +48,15 @@ int writeTime(int64_t uptime) {
   int bytes_written;
    
   bytes_written = i2c_write_blocking(I2C, FRAM_ADDR, data, 10, false);
-
-  LOG_DEBUG("Wrote %d bytes to i2c address: %x\n", bytes_written, FRAM_ADDR);
-  LOG_DEBUG("Wrote %02x,%02x,%02x,%02x,%02x,%02x,%02x,%02x,%02x,%02x \n", \
-      data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9]);
-  return 0;
+  if (bytes_written != 10){
+    LOG_DEBUG("Write failed: bytes_written=%d\n", bytes_written);
+    return -1;
+  } else {
+    //LOG_DEBUG("Wrote %02x,%02x,%02x,%02x,%02x,%02x,%02x,%02x,%02x,%02x \n", \
+               data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9]);
+    //LOG_DEBUG("write success\n");
+    return 0;
+  }
 }
 
 int64_t readTime() {
@@ -64,10 +65,14 @@ int64_t readTime() {
 
   i2c_write_blocking(I2C, FRAM_ADDR, UPTIME_ADDR, 2, true);
   bytes_read = i2c_read_blocking(I2C, FRAM_ADDR, data, 8, false);
-  LOG_DEBUG("read %d from address: %x\n", bytes_read, FRAM_ADDR);
-  LOG_DEBUG("Read %02x,%02x,%02x,%02x,%02x,%02x,%02x,%02x \n", \
-      data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7]);
-
+  if (bytes_read < 0){
+    LOG_DEBUG("Read failed.\n");
+    return -1;
+  } else {
+    LOG_DEBUG("Read %02x,%02x,%02x,%02x,%02x,%02x,%02x,%02x \n", \
+               data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7]);
+  }
+  // prepare output
   int64_t uptime;
   for (int i=0; i<8; i++) uptime = (uptime << 8) | data[i-7];
   return uptime;
@@ -92,10 +97,29 @@ int main() {
   char state = 0;
 #endif
   int64_t uptime;
-
   init_i2c_bus();
 
-  sleep_ms(5000);
+#ifndef PROD
+  int ret;
+  sleep_ms(4000);
+  uptime = 0;
+  ret = writeTime(uptime);
+  if (ret < 0){
+    LOG_DEBUG("reset failed\n");
+    return 1;
+  }
+  LOG_DEBUG("wrote reset to FRAM\n");
+  flipLED(1);
+  sleep_ms(200);
+  flipLED(0);
+  sleep_ms(200);
+  flipLED(1);
+  sleep_ms(200);
+  //for(;;) tight_loop_contents();
+#endif
+
+  int64_t lifetime = readTime();
+  LOG_DEBUG("lifetime: %lld sec\n", lifetime);
 
   while (true) {
 #ifndef PROD
@@ -103,17 +127,13 @@ int main() {
 #endif
     sleep_ms(DELAY);
 
-    int64_t lifetime = readTime();
-    //lifetime = 0;
-    LOG_DEBUG("lifetime: %lld sec\n", lifetime);
     if(dead(lifetime)){
       LOG_DEBUG("dead\n");
     } else {
       uptime = to_ms_since_boot(get_absolute_time()) / 1000;
-      LOG_DEBUG("up-time: %d sec\n", uptime);
+      LOG_DEBUG("up-time: %d sec\r\n", uptime);
       writeTime(uptime);
     }
-
 
   }
 }
